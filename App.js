730afc676d2d6666d98506350f260df928e4b0f8
zahidalidev <engrzahid612@gmail.com>
2020-10-18 06:07:33 +0500

    :robot: Splash Screen complete without white flash

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -24,7 +24,14 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import SplashScreen from "react-native-splash-screen";
+
 const App: () => React$Node = () => {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  });
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#4f6d7a" />
